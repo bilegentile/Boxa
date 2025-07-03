f789000e11784d6270393f9f56b964af70d10102
bilegentile <76962962+BileGentile@users.noreply.github.com>
2025-07-03 23:56:11 +0200

    Initial Commit

---
+++ Boxa/BoxaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxaApp.swift
+//  Boxa
+//
+//  Created by Beniamino Gentile on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BoxaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
